39d33e268e6274689d7379c440c69bfd4293d993
luckydamami <luckydamamii2410@gmail.com>
2026-06-14 00:40:01 +0530

    Add mobile menu functionality and update footer styles

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-  
+const mobileOpenButton = document.querySelector("#menu-open-button");
+const mobileCloseButton = document.querySelector("#menu-close-button");
+
+mobileOpenButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu");
+});
