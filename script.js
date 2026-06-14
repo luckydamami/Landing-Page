85b12adb40d5d5d5c3fb5434cde8113e2fa261f6
luckydamami <luckydamamii2410@gmail.com>
2026-06-14 12:50:35 +0530

    Refactor mobile menu functionality and update styles for responsiveness, Final project is completed

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const mobileOpenButton = document.querySelector("#menu-open-button");
-const mobileCloseButton = document.querySelector("#menu-close-button");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+const navMenu = document.querySelector(".nav-menu");
 
-mobileOpenButton.addEventListener("click", () => {
-  document.body.classList.toggle("show-mobile-menu");
+menuOpenButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  navMenu.classList.add("nav-menu--active");
+  document.body.classList.add("menu-open");
+});
+
+menuCloseButton.addEventListener("click", () => {
+  navMenu.classList.remove("nav-menu--active");
+  document.body.classList.remove("menu-open");
+});
+
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("nav-menu--active");
+    document.body.classList.remove("menu-open");
+  });
 });
